f77800b1e8b0e582021e844bc3c0301f2551d779
sutantoc <165302919+sutantoc@users.noreply.github.com>
2024-04-11 02:23:43 +0000

    edits to href

--- dice.rb
+++ dice.rb
@@ -10,14 +10,12 @@ BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
 
 get ("/") do
   "<h1> Dice Roll </h1>
-  <p>
   <ul>
-  <li> <a href=/dice/2/6> Roll two 6-sided dice </a></li>
-  <li> <a href=/dice/2/10> Roll two 10-sided dice </a></li>
-  <li> <a href=/dice/1/20> Roll one 20-sided die </a></li>
-  <li> <a href=/dice/5/4>Rol five 4-sided dice </a></li>
-  </ul>
-  </p>"
+  <li><a href=\"/dice/2/6\">Roll two 6-sided dice</a></li>
+  <li><a href=\"/dice/2/10\">Roll two 10-sided dice</a></li>
+  <li><a href=\"/dice/1/20\">Roll one 20-sided die</a></li>
+  <li><a href=\"/dice/5/4\">Roll five 4-sided dice</a></li>
+  </ul>"
 end
 
 get ("/zebra") do
