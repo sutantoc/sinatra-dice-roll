625b48f58bbcc3fd67bf693bedd705b0f6897c97
sutantoc <165302919+sutantoc@users.noreply.github.com>
2024-04-11 02:16:58 +0000

    home page links

--- dice.rb
+++ dice.rb
@@ -2,7 +2,15 @@ require "sinatra"
 require "sinatra/reloader"
 
 get ("/") do
-  "Hello World"
+  "<h1> Dice Roll </h1
+  <p>
+  <ul>
+  <li> <a href=/dice/2/6> Roll two 6-sided dice </a></li>
+  <li> <a href=/dice/2/10> Roll two 10-sided dice </a></li>
+  <li> <a href=/dice/1/20> Roll one 20-sided die </a></li>
+  <li> <a href=/dice/5/4>Rol five 4-sided dice </a></li>
+  </ul>
+  </p>"
 end
 
 get ("/zebra") do
@@ -57,4 +65,3 @@ get ("/dice/5/4") do
   "<h1> 5d4 </h1>
   <p> #{outcome} </p>"
 end
-
