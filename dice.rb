e9f86d70280d5097aee0e374ce308086051e012d
sutantoc <165302919+sutantoc@users.noreply.github.com>
2024-04-11 02:49:57 +0000

    red green text 1 20

--- dice.rb
+++ dice.rb
@@ -41,9 +41,9 @@ get ("/dice/2/10") do
 end
 
 get ("/dice/1/20") do
-  first_die = rand(1..20)
+  @die = rand(1..20)
 
-  @outcome = "You rolled a #{first_die} for a total of #{first_die}."
+  @outcome = "You rolled a #{@die}."
   
   erb(:one_twenty)
 end
