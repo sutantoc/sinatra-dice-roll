24ff681bc5857735da65b72efa65c6dfd8bda54a
sutantoc <165302919+sutantoc@users.noreply.github.com>
2024-04-11 02:55:11 +0000

    one hundred six

--- dice.rb
+++ dice.rb
@@ -60,3 +60,15 @@ get ("/dice/5/4") do
 
   erb(:five_four)
 end
+
+get ("/dice/100/6") do
+  @rolls = []
+
+  100.times do
+    die = rand(1..6)
+    @rolls.push(die)
+  end
+
+  erb(:one_hundred_six)
+end
+
