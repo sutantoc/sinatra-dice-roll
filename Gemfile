bc708b7fe0f5a2ea6e989e6afab5460ade234660
sutantoc <165302919+sutantoc@users.noreply.github.com>
2024-04-11 02:20:00 +0000

    error page

--- Gemfile
+++ Gemfile
@@ -3,7 +3,9 @@ source "https://rubygems.org"
 gem "puma"
 gem "sinatra"
 
-
 gem "rackup", "~> 2.1"
 
 gem "sinatra-contrib"
+
+gem "better_errors"
+gem "binding_of_caller"
--- dice.rb
+++ dice.rb
@@ -1,8 +1,15 @@
 require "sinatra"
 require "sinatra/reloader"
+require "better_errors"
+require "binding_of_caller"
+
+# Need this configuration for better_errors
+use(BetterErrors::Middleware)
+BetterErrors.application_root = __dir__
+BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
 
 get ("/") do
-  "<h1> Dice Roll </h1
+  "<h1> Dice Roll </h1>
   <p>
   <ul>
   <li> <a href=/dice/2/6> Roll two 6-sided dice </a></li>
